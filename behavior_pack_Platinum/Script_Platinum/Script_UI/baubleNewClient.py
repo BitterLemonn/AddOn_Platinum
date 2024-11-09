8b55e16404ad483daa577ff071d4e153dd1883d0
bitterlemon <873811906@qq.com>
2024-11-09 11:44:45 +0800

    [fix] 修复减少饰品耐久至损坏时数据不包含slotId的问题

--- behavior_pack_Platinum/Script_Platinum/Script_UI/baubleNewClient.py
+++ behavior_pack_Platinum/Script_Platinum/Script_UI/baubleNewClient.py
@@ -1209,6 +1209,7 @@ class ChangeBaubleUtil(object):
     @AllowCall
     def DecreaseBaubleDurability(num, slotName):
         baubleInfo = GlobalData.baubleDict[slotName]
+        slotIndex = int(re.findall(r"\d+", slotName)[-1]) if len(re.findall(r"\d+", slotName)) > 0 else 0
         if len(baubleInfo) > 0:
             comp = clientApi.GetEngineCompFactory().CreateItem(levelId)
             itemInfo = comp.GetItemBasicInfo(baubleInfo["newItemName"], baubleInfo["newAuxValue"])
@@ -1219,7 +1220,7 @@ class ChangeBaubleUtil(object):
                     comp = clientApi.GetEngineCompFactory().CreateCustomAudio(levelId)
                     comp.PlayCustomMusic("random.break", (0, 0, 0), 0.8, 0.8, False, playerId)
                     # 广播饰品脱下
-                    BaubleUnequippedBroadcaster(BaubleConfig.SlotName2TypeDict[slotName], baubleInfo)
+                    BaubleUnequippedBroadcaster(BaubleConfig.SlotName2TypeDict[slotName], baubleInfo, slotIndex)
                     # 移除玩家饰品栏中的饰品
                     baubleInfo = {}
                 GlobalData.baubleDict[slotName] = baubleInfo
